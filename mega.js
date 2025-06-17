68c6898ee20e76cec87bc61dc9ecd474f69a63ef
Mr Jestor <108587930+wijesuriya2017@users.noreply.github.com>
2025-06-17 22:58:31 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "ja.m.es.buna.g.na@gmail.com",
+  password: "SATH@20170501",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
